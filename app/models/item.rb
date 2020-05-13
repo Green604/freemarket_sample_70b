b8cdb88d8701e7b71ba01374a407ae224abe10b6
GreenMK <mmmkanzawa@gmail.com>
2020-05-13 23:23:58 +0900

    modify model item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,22 @@
 class Item < ApplicationRecord
+  has_one :seller, dependent: :destroy
+  has_one :buyer
+  has_one :selling_status, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :users, through: :favorites
+  has_many :comments, dependent: :destroy
+  has_many :users, through: :comments
+  has_many :images, dependent: :destroy
+  belongs_to :brand
+  belongs_to :category
+  belongs_to :shipping
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :condition, presence: true
+  validates :category, presence: true
+  validates :shipping, presence: true
+
+  enum condition: [ :new, :no_use, :clean, :litte_dirty, :dirty, :bad ]
 end
--- db/migrate/20200505073555_create_items.rb
+++ db/migrate/20200505073555_create_items.rb
@@ -1,11 +1,13 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.string :title
-      t.string :string
-      t.string :text
-      t.string :text
-
+      t.string  :name,         null: false, index: true
+      t.text    :description,  null: false  
+      t.integer :price,        null: false   
+      t.integer :condition,    null: false 
+      t.references :brand,    foreign_key: true
+      t.references :category, foreign_key: true
+      t.references :shipping, foreign_key: true
       t.timestamps
     end
   end
