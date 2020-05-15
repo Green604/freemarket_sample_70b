a22f0426b3033f5de93d8da637a7956cbeda08d5
Maioon-png <mi_mkb.12_5@icloud.com>
2020-05-15 18:47:17 +0900

    setting routes payments

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root "items#index"
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :payments, only: [:index, :new, :create]
 
 end
