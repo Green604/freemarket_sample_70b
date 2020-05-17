9825b94657bad96b3891c6f13439ab73a2a25f32
Maioon-png <mi_mkb.12_5@icloud.com>
2020-05-17 17:08:22 +0900

    change form_with

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,7 +17,7 @@ class Item < ApplicationRecord
   validates :condition, presence: true
   validates :category, presence: true
   validates :shipping, presence: true
-  validates :brand, presence: true
+  # validates :brand, presence: true
 
   enum condition: [ :brand_new, :no_use, :clean, :litte_dirty, :dirty, :bad ]
 end
