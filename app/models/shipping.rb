d4283b3ecc6309d14cf3b0adf15bf3a53e8dd1d8
naota7118 <naota7118@gmail.com>
2020-05-21 19:33:07 +0900

    make shippingway.rb

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -4,5 +4,8 @@ class Shipping < ApplicationRecord
   validates :shipping_area, presence: true
   validates :shipping_day,  presence: true
   validates :shipping_fee,  presence: true
+  validates :shippingway_id,  presence: true
   
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :shippingway
 end
