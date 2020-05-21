1742b21f111b0efd092f5c33c68298f4efe7af54
naota7118 <naota7118@gmail.com>
2020-05-21 21:57:25 +0900

    change column from shipping to shippinway

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_122724) do
+ActiveRecord::Schema.define(version: 2020_05_21_125345) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -120,7 +120,7 @@ ActiveRecord::Schema.define(version: 2020_05_21_122724) do
     t.string "shipping_fee", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "shipping_id"
+    t.integer "shippingway_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
