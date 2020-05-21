5e169df8bf57aa8e0bb56cc1696467c8d72019db
Maioon-png <mi_mkb.12_5@icloud.com>
2020-05-21 16:17:39 +0900

    add show users creditcard infomation

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,11 +2,32 @@ class PaymentsController < ApplicationController
   require "payjp"
 
   def index
+    @card = Payment.where(user_id: current_user).first
+    if @card.present?
+      Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
+      customer = Payjp::Customer.retrieve(@card.payjp_id)
+      @card_info = customer.cards.retrieve(customer.default_card)
+      @card_brand = @card_info.brand
+      @card_month = @card_info.exp_month
+      @card_year = @card_info.exp_year % 1000
+
+      case @card_brand
+      when "Visa" then
+        @card_image = "cc-visa fa-2x"
+      when "MasterCard" then
+        @card_image = "cc-mastercard fa-2x"
+      when "JCB" then
+        @card_image = "c-jcb fa-2x"
+      when "American Express" then
+        @card_image = "cc-amex fa-2x"
+      end
+
+    end
   end
 
   def new
-    @card = Payment.where(user_id: current_user.id)
-    redirect_to action: "index" if @card.present?
+    # @card = Payment.where(user_id: current_user.id)
+    # redirect_to action: "index" if @card.present?
   end
 
   def create
