f80a4672a852d75be85ee1224d4cdb58f4c925f6
rnom <rnomura8@gmail.com>
2020-05-24 16:41:55 +0900

    modify routes.rb according to advice by TOMARUMARU
    ・delete comment-out in _mainContents.html.haml
    ・set itemsController except with edit and show actions
    ・delete routing for imageController

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,6 @@ Rails.application.routes.draw do
   end
 
   root "items#index"
-  resources :items do
-    resources :images
-  end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :items, except: [:edit, :show]
+  
 end
