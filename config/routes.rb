98377d3c6a0776d41187b2620eabbce3eabc1cd9
rnom <rnomura8@gmail.com>
2020-05-29 17:45:11 +0900

    show each item in my-page

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,9 @@ Rails.application.routes.draw do
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
     end
+
   end
+
+  resources :users, only: :show
 
 end
