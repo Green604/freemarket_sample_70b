b94617317a9bad18beb56baee052c3c681406098
rnom <rnomura8@gmail.com>
2020-05-29 13:36:14 +0900

    delete comment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,10 +11,8 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @item.images.find(params[:id])
-    # 商品の親カテゴリーのIDを取得後、そのIDでカテゴリーテーブルから親カテゴリーの名前を取得
     category_parent = @item.parent_category_id
     @category = Category.find(category_parent)
-    # 商品の子カテゴリーのIDを取得後、そのIDでカテゴリーテーブルから子カテゴリーの名前を取得
     category_child = @item.child_category_id
     @category_child = Category.find(category_child)
   end
@@ -66,17 +64,13 @@ class ItemsController < ApplicationController
   end
 
   def get_category_children
-    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
     @category_children = Category.find(params[:parent_id]).children
   end
 
-  # 子カテゴリーが選択された後に動くアクション。 ajaxからハッシュで子要素のIDを受け取る{child_id: childId}
   def get_category_grandchildren
-    #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
     @category_grandchildren = Category.find(params[:child_id]).children
   end
 
-  # 親カテゴリー、子カテゴリー、孫カテゴリーのIDを保存できるようにitemsテーブルにカラムを追加し詳細ページでカテゴリーを表示できるようにしている
   private
   def item_params
     params.require(:item).permit(:name, :description, :price, :condition, :brand_id, :parent_category_id, :child_category_id, :category_id, :shipping_id, images_attributes: [:image], shipping_attributes: [:shipping_day, :shipping_fee, :shippingway_id, :shippingarea_id])
