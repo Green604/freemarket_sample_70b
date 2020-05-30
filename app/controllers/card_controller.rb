c3cb8cd073d3877306d6b52efdfabe6047f80929
Maioon-png <mi_mkb.12_5@icloud.com>
2020-05-30 22:12:20 +0900

    add card views and show action in card_controller

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -50,3 +50,30 @@ class CardController < ApplicationController
     end
   end
 end
+
+# 岩崎追加
+  # def show #Cardのデータpayjpに送り情報を取り出します
+  #   card = Payment.where(user_id: current_user.id).first
+  #   if card.present?
+  #     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+  #     customer = Payjp::Customer.retrieve(card.customer_id)
+  #     @default_card_information = customer.cards.retrieve(card.card_id)
+  #     @card_brand = @default_card_information .brand
+  #     @card_month = @default_card_information .exp_month
+  #     @card_year = @default_card_information .exp_year % 1000
+  
+  #     case @card_brand
+  #     when "Visa" then
+  #       @card_image = "cc-visa fa-2x"
+  #     when "MasterCard" then
+  #       @card_image = "cc-mastercard fa-2x"
+  #     when "JCB" then
+  #       @card_image = "c-jcb fa-2x"
+  #     when "American Express" then
+  #       @card_image = "cc-amex fa-2x"
+  #     end      
+  #   end
+  # end
+
+
+
