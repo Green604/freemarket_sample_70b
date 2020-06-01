e657405d4e4b06b2d0572ef04110af4e7bc4c34d
kyouheisunaga <sunagatheflame@gmail.com>
2020-06-02 00:02:01 +0900

    create mypage

--- config/routes.rb
+++ config/routes.rb
@@ -35,12 +35,6 @@ Rails.application.routes.draw do
     end
   end
 
-  # resources :purchase, only: [:index] do
-  #   collection do
-  #     get 'index', to: 'purchase#index'
-  #     post 'pay', to: 'purchase#pay'
-  #     get 'done', to: 'purchase#done'
-  #   end
-  # end
+  resources :users, only: :show
 
 end
