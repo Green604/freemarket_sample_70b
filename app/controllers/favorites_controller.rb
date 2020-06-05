0956d1a83604340d472c039dd39305cc6f2c888d
naota7118 <naota7118@gmail.com>
2020-06-05 15:23:59 +0900

    modify favoritesController

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,15 @@
 class FavoritesController < ApplicationController
+
+  def index
+    @favoriteItems = []
+    favorites = current_user.favorites.ids          #current_userのfavorite_idを取得
+    favorites.each do |f|                           #current_userがお気に入りした数だけ１つずつ取り出して行われる
+      favoriteItem_id = Favorite.find(f).item_id    #user.id=current_user.idに対応するitem_idをfavoriteテーブルで取り出す
+      @favoriteItem = Item.find(favoriteItem_id)    #1つ上で取り出したitem_idに当たるitemをitemテーブルで取り出す
+      @favoriteItems << @favoriteItem               #配列に入れる
+    end
+  end
+
   def create
     @favorite = current_user.favorites.create(item_id: params[:item_id])
     # @favorite = Favorite.new(item_id: params[:item_id], user_id: current_user.id)
@@ -23,9 +34,6 @@ class FavoritesController < ApplicationController
     respond_to do |format|
       format.json
     end
-  end
-
-  def show
   end
 
 end
