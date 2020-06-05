aa1ff435aa9954a8033ce5b5ef35fa583f1b4340
naota7118 <naota7118@gmail.com>
2020-06-05 15:22:45 +0900

    add index in route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   resources :items do
 
-    resources :favorites, only: [:create, :destroy, :show]
+    resources :favorites, only: [:create, :destroy, :index]
 
     collection do
       get 'get_category_children', defaults: { format: 'json' }
