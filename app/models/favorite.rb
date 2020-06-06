b79e80a2d74dcdd87a7f922b867013feacffb300
naota7118 <naota7118@gmail.com>
2020-06-06 12:22:38 +0900

    put together validation

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,7 +2,5 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :item
   validates_uniqueness_of :item_id, scope: :user_id
-
-  validates :item_id, presence: true
-  validates :user_id, presence: true
+  validates :item_id, :user_id, presence: true
 end
