5f0ed1ff542bc8ca04753cd48ab5132c6d6c3c09
naota7118 <naota7118@gmail.com>
2020-06-06 12:24:23 +0900

    put together validation

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,7 +1,4 @@
 class Payment < ApplicationRecord
   belongs_to :user
-
-  validates :user_id, presence: true
-  validates :card_id, presence: true
-  validates :customer_id, presence: true
+  validates :user_id, :card_id, :customer_id, presence: true
 end
