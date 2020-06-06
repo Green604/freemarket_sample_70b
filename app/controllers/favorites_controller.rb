a6aaa5157a2f3a2d13e098b83ac58fb855673869
naota7118 <naota7118@gmail.com>
2020-06-06 12:03:26 +0900

    delete commentout

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,25 +2,20 @@ class FavoritesController < ApplicationController
 
   def index
     @favoriteItems = []
-    favorites = current_user.favorites.ids          #current_userのfavorite_idを取得
-    favorites.each do |f|                           #current_userがお気に入りした数だけ１つずつ取り出して行われる
-      favoriteItem_id = Favorite.find(f).item_id    #user.id=current_user.idに対応するitem_idをfavoriteテーブルで取り出す
-      @favoriteItem = Item.find(favoriteItem_id)    #1つ上で取り出したitem_idに当たるitemをitemテーブルで取り出す
-      @favoriteItems << @favoriteItem               #配列に入れる
+    favorites = current_user.favorites.ids
+    favorites.each do |f|
+      favoriteItem_id = Favorite.find(f).item_id
+      @favoriteItem = Item.find(favoriteItem_id)
+      @favoriteItems << @favoriteItem
     end
   end
 
   def create
     @favorite = current_user.favorites.create(item_id: params[:item_id])
-    # @favorite = Favorite.new(item_id: params[:item_id], user_id: current_user.id)
-
-    # redirect_to item_path(@favorite.item.id)
     if @favorite.save
       @favoriteCounts = Favorite.where(item_id: params[:item_id])
-      respond_to do |format| # リクエストされたformatによって処理を分ける
-        # format.html { redirect_to item_path(@favorite.item.id) }
-        format.json  # jsファイルで作成したfavorite(@favorite)を使用するため、renderメソッドを使用して、作成したfavoriteをjson形式で返す
-        # format.json { render json :@favorite } 7行目の記述が間違っていたので修正した
+      respond_to do |format| 
+        format.json  
       end 
     else
       redirect_to item_path(@favorite.item.id)
