84f45532626fb9db2f5d7e050a53a6fee6c14d02
naota7118 <naota7118@gmail.com>
2020-06-11 20:45:55 +0900

    edit item.js to edit items

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,138 +1,145 @@
 $(function() {
+  $(function() {
   // 画像用のinputを生成する関数
-  function buildHTML(index) {
-    var html = `<div class="preview-box" id="preview-box__${index}">
-                  <div data-index="${index}" class="upper-box">
-                    <img src="" alt="preview" class="upload-image">
-                  </div>
-                  <div class="lower-box">
-                    <div class="delete-box" id="delete_btn_${index}">
-                      <span>削除</span>
+    function buildHTML(index) {
+      var html = `<div class="preview-box" id="preview-box__${index}">
+                    <div data-index="${index}" class="upper-box">
+                      <img src="" alt="preview" class="upload-image">
                     </div>
-                  </div>
-                </div>`;
-    return html;
-  }
+                    <div class="lower-box">
+                      <div class="delete-box" id="delete_btn_${index}">
+                        <span>削除</span>
+                      </div>
+                    </div>
+                  </div>`;
+      return html;
+    }
 
-  // ラベルのwidth操作  ラベルの横幅を変える関数
-  function setLabel() {
-    //プレビューボックスのwidthを取得し、maxから引くことでラベルのwidthを決定
-    var prevContent = $('.label-content').prev(); //prevメソッドは直前のhtmlを取得。label-contentクラスを取得して変数に代入
-    labelWidth = (620 - $(prevContent).css('width').replace(/[^0-9]/g, '')); // label-contentクラスの横幅をreplaceで置き換えて、620からそれを引いたものを変数に代入する
-    $('.label-content').css('width', labelWidth); // label-contentクラスの横幅を620-xに変更する
-  }
-
-  // プレビューの追加
-  $(document).on('change', '.hidden-field', function() { //id=image-boxが対象要素。フォーム部品の状態に何らかの変化があったときに発動するchangeイベント。js-fileはセレクタ。セレクタは対象要素内でさらに指定したセレクタからのイベントだけを確認できるようにする
-    setLabel(); //label-contentクラスの横幅を変えるメソッドを呼び出す(＝横幅を変える処理を実行する)
-    //js-fileのidの数値のみ取得（id="item_images_attributes_0_image"の0だけ取得するということ。0〜9の数字を取得）
-    var id = $(this).attr('id').replace(/[^0-9]/g, '');
-    //labelボックスのidとforを更新
-    $('.label-box').attr({id: `label-box--${id}`,for: `item_images_attributes_${id}_image`});
-    //選択したfileのオブジェクトを取得
-    var file = this.files[0];
-    var reader = new FileReader(); //FileReaderオブジェクトの生成
-    //readAsDataURLで指定したFileオブジェクトを読み込む
-    reader.readAsDataURL(file); 
-    //読み込み時に発火するイベント onloadメソッドは読み込みが完了したら実行する
-    reader.onload = function() {
-      var image = this.result; //直前に実行したイベントが返した値を取得する
-      //プレビューが元々なかった場合はhtmlを追加
-      if ($(`#preview-box__${id}`).length == 0) {
-        var index = $('.preview-box').length; //preview-boxの数を数えて変数に代入
-        var html = buildHTML(id); //プレビューを生成する関数を変数に代入
-        //ラベルの直前のプレビュー群にプレビューを追加
-        var prevContent = $('.label-content').prev(); //prevメソッドは直前のhtmlを取得。label-contentクラスを取得して変数に代入
-        $(prevContent).append(html); 
+    // 投稿編集時
+    //items/:i/editページへリンクした際のアクション=======================================
+    if (window.location.href.match(/\/items\/\d+\/edit/)){
+      //登録済み画像のプレビュー表示欄の要素を取得する
+      var prevContent = $('.label-content').prev();
+      labelWidth = (620 - $(prevContent).css('width').replace(/[^0-9]/g, ''));
+      $('.label-content').css('width', labelWidth);
+      //プレビューにidを追加
+      $('.preview-box').each(function(index, box){
+        $(box).attr('id', `preview-box__${index}`);
+      })
+      //削除ボタンにidを追加
+      $('.delete-box').each(function(index, box){
+        $(box).attr('id', `delete_btn_${index}`);
+      })
+      var count = $('.preview-box').length;
+      //プレビューが5あるときは、投稿ボックスを消しておく
+      if (count == 5) {
+        $('.label-content').hide();
       }
+    }
+    //=============================================================================
 
-      //イメージを追加
-      $(`#preview-box__${id} img`).attr('src', `${image}`); //#preview-box__${id} imgのsrc属性を取得してimageに変更する
-      var index = $('.preview-box').length; //preview-boxの数を数えて変数に代入
-      //プレビューが5個あったらラベルを隠す。ここはあとで5を10に変えてもいいかも
-      if (index == 5) {
-        $('.label-content').hide(); //対象要素を隠す
-      }
 
-      //ラベルのwidth操作
+    // ラベルのwidth操作  ラベルの横幅を変える関数
+    function setLabel() {
+      //プレビューボックスのwidthを取得し、maxから引くことでラベルのwidthを決定
+      var prevContent = $('.label-content').prev(); //prevメソッドは直前のhtmlを取得。label-contentクラスを取得して変数に代入
+      labelWidth = (620 - $(prevContent).css('width').replace(/[^0-9]/g, '')); // label-contentクラスの横幅をreplaceで置き換えて、620からそれを引いたものを変数に代入する
+      $('.label-content').css('width', labelWidth); // label-contentクラスの横幅を620-xに変更する
+    }
+
+    // プレビューの追加
+    $(document).on('change', '.hidden-field', function() { //id=image-boxが対象要素。フォーム部品の状態に何らかの変化があったときに発動するchangeイベント。js-fileはセレクタ。セレクタは対象要素内でさらに指定したセレクタからのイベントだけを確認できるようにする
       setLabel(); //label-contentクラスの横幅を変えるメソッドを呼び出す(＝横幅を変える処理を実行する)
-      //ラベルのidとforの値を変更
-      if(index < 5){
-        //プレビューの数でラベルのオプションを更新する
-        $('.label-box').attr({id: `label-box--${index}`,for: `item_images_attributes_${index}_image`}); //これはたぶんOK
-      }
-    }
-  });
-
-  //------------------------------------------------------------------------------
-
-  // file_fieldのnameに動的なindexをつける為の配列
-  // let fileIndex = [1,2,3,4,5,6,7,8,9,10];
-
-  // 既に使われているindexを除外
-  // lastIndex = $('.js-file_group:last').data('index'); //data属性の取得。引数にindexとしているのでdata-indexだけを取得する
-  // fileIndex.splice(0, lastIndex);
-
-  // $('.hidden-destroy').hide(); //対象要素を隠す
-
-  // $('#image-box').on('change', '.js-file', function(e) {
-  //   console.log('hello!');
-  //   const targetIndex = $(this).parent().data('index');
-  //   // ファイルのブラウザ上でのURLを取得する
-  //   const file = e.target.files[0];
-  //   const blobUrl = window.URL.createObjectURL(file);
-
-  //   // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
-  //   if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
-  //     img.setAttribute('src', blobUrl);
-  //   } else {  // 新規画像追加の処理
-  //     $('#previews').append(buildHTML(targetIndex, blobUrl));
-  //     // fileIndexの先頭の数字を使ってinputを作る
-  //     $('#image-box').append(buildFileField(fileIndex[0]));
-  //     fileIndex.shift();
-  //     // 末尾の数に1足した数を追加する
-  //     fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
-  //   }
-  // });
-
-  // $('#image-box').on('click', '.js-remove', function() {
-  //   const targetIndex = $(this).parent().data('index')
-  //   // 該当indexを振られているチェックボックスを取得する
-  //   const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
-  //   // もしチェックボックスが存在すればチェックを入れる
-  //   if (hiddenCheck) hiddenCheck.prop('checked', true);
-
-  //   $(this).parent().remove();
-  //   $(`img[data-index="${targetIndex}"]`).remove();
-  //   // 画像入力欄が0個にならないようにしておく
-  //   if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
-  // });
-
-
-  // 画像の削除
-  $(document).on('click', '.delete-box', function() {
-    var index = $('.preview-box').length;
-    setLabel(index);
-    //item_images_attributes_${id}_image から${id}に入った数字のみを抽出
-    var id = $(this).attr('id').replace(/[^0-9]/g, '');
-    //取得したidに該当するプレビューを削除
-    $(`#preview-box__${id}`).remove();
-    console.log("new")
-    //フォームの中身を削除 
-    $(`#item_images_attributes_${id}_image`).val("");
-
-    //削除時のラベル操作
-    var index = $('.preview-box').length;
-    //5個めが消されたらラベルを表示
-    if (index == 4) {
-      $('.label-content').show();
-    }
-    setLabel(index);
-
-    if(id < 5){
-      //削除された際に、空っぽになったfile_fieldをもう一度入力可能にする
+      //js-fileのidの数値のみ取得（id="item_images_attributes_0_image"の0だけ取得するということ。0〜9の数字を取得）
+      var id = $(this).attr('id').replace(/[^0-9]/g, '');
+      //labelボックスのidとforを更新
       $('.label-box').attr({id: `label-box--${id}`,for: `item_images_attributes_${id}_image`});
-    }
+      //選択したfileのオブジェクトを取得
+      var file = this.files[0];
+      var reader = new FileReader(); //FileReaderオブジェクトの生成
+      //readAsDataURLで指定したFileオブジェクトを読み込む
+      reader.readAsDataURL(file); 
+      //読み込み時に発火するイベント onloadメソッドは読み込みが完了したら実行する
+      reader.onload = function() {
+        var image = this.result; //直前に実行したイベントが返した値を取得する
+        //プレビューが元々なかった場合はhtmlを追加
+        if ($(`#preview-box__${id}`).length == 0) {
+          var index = $('.preview-box').length; //preview-boxの数を数えて変数に代入
+          var html = buildHTML(id); //プレビューを生成する関数を変数に代入
+          //ラベルの直前のプレビュー群にプレビューを追加
+          var prevContent = $('.label-content').prev(); //prevメソッドは直前のhtmlを取得。label-contentクラスを取得して変数に代入
+          $(prevContent).append(html); 
+        }
+        //イメージを追加
+        $(`#preview-box__${id} img`).attr('src', `${image}`); //#preview-box__${id} imgのsrc属性を取得してimageに変更する
+        var index = $('.preview-box').length; //preview-boxの数を数えて変数に代入
+        //プレビューが5個あったらラベルを隠す。ここはあとで5を10に変えてもいいかも
+        if (index == 5) {
+          $('.label-content').hide(); //対象要素を隠す
+        }
+
+        //=============================================================================
+        //プレビュー削除したフィールドにdestroy用のチェックボックスがあった場合、チェックを外す=============
+        if ($(`#item_images_attributes_${id}__destroy`)){
+          $(`#item_images_attributes_${id}__destroy`).prop('checked',false);
+        } 
+        //=============================================================================
+
+
+        //ラベルのwidth操作
+        setLabel(); //label-contentクラスの横幅を変えるメソッドを呼び出す(＝横幅を変える処理を実行する)
+        //ラベルのidとforの値を変更
+        if(index < 5){
+          //プレビューの数でラベルのオプションを更新する
+          $('.label-box').attr({id: `label-box--${index}`,for: `item_images_attributes_${index}_image`}); //これはたぶんOK
+        }
+      }
+    });
+
+    // 画像の削除
+    $(document).on('click', '.delete-box', function() {
+      var index = $('.preview-box').length;
+      setLabel(index);
+      //item_images_attributes_${id}_image から${id}に入った数字のみを抽出
+      var id = $(this).attr('id').replace(/[^0-9]/g, '');
+      //取得したidに該当するプレビューを削除
+      $(`#preview-box__${id}`).remove();
+
+      //新規登録時と編集時の場合分け==========================================================
+
+      //新規投稿時
+      //削除用チェックボックスの有無で判定
+      if ($(`#item_images_attributes_${id}__destroy`).length == 0) {
+        //フォームの中身を削除 
+        $(`#item_images_attributes_${id}_image`).val("");
+        //削除時のラベル操作
+        var index = $('.preview-box').length;
+        //5個めが消されたらラベルを表示
+        if (index == 4) {
+          $('.label-content').show();
+        }
+        setLabel(index);
+        if(id < 5){
+          //削除された際に、空っぽになったfile_fieldをもう一度入力可能にする
+          $('.label-box').attr({id: `label-box--${id}`,for: `item_images_attributes_${id}_image`});
+        }
+      } else {
+        //投稿編集時
+        $(`#item_images_attributes_${id}__destroy`).prop('checked',true);
+        //5個めが消されたらラベルを表示
+        if (index == 5) {
+          $('.label-content').show();
+        }
+
+        //ラベルのwidth操作
+        setLabel();
+        //ラベルのidとforの値を変更
+        //削除したプレビューのidによって、ラベルのidを変更する
+        if(id < 5){
+          $('.label-box').attr({id: `label-box--${id}`,for: `item_images_attributes_${id}_image`});
+        }
+      }
+    });
   });
 });
 
