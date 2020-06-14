33fe98c05575a32d25c3c80b8a5fd3bbbc354674
Maioon-png <mi_mkb.12_5@icloud.com>
2020-06-14 15:43:00 +0900

    add comments

--- app/helpers/with_error_form_builder.rb
+++ app/helpers/with_error_form_builder.rb
@@ -1,9 +1,10 @@
 class WithErrorFormBuilder < ActionView::Helpers::FormBuilder
+
   # ①エラーメッセージを含んだ要素を生成してあげる記述
   def pick_errors(attribute)
     return nil if @object.nil? || (messages = @object.errors.messages[attribute]).nil?
     
-    # collect=mapメソッド　各要素に対してブロックを評価した結果を全て含む配列を返します。
+    # collect=mapメソッド…各要素に対してブロックを評価した結果を全て含む配列を返す
     lis = messages.collect do |message|
       %{<li>#{@object.errors.full_message(attribute, message)}</li>}
     end.join
