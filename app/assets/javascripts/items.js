2c83add75abdb19c3edbb2378edf40f8981cae10
naota7118 <naota7118@gmail.com>
2020-06-25 03:16:51 +0900

    add addlabel-content

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,18 +1,48 @@
 $(function() {
   $(function() {
-  // 画像用のinputを生成する関数
+    // 画像用のinputを生成する関数
     function buildHTML(count) {
       var html = `<div class="preview-box" id="preview-box__${count}">
-                    <div data-index="${count}" class="upper-box">
-                      <img src="" alt="preview" class="upload-image">
-                    </div>
-                    <div class="lower-box">
-                      <div class="delete-box" id="delete_btn_${count}">
-                        <span>削除</span>
-                      </div>
-                    </div>
-                  </div>`;
+      <div data-index="${count}" class="upper-box">
+      <img src="" alt="preview" class="upload-image">
+      </div>
+      <div class="lower-box">
+      <div class="delete-box" id="delete_btn_${count}">
+      <span>削除</span>
+      </div>
+      </div>
+      </div>`;
       return html;
+    }
+    
+    //6〜10枚目のプレビュー画像のためのラベル作る関数
+    function addLabelHTML(id) {
+      var labelhtml =  `<div class="addlabel-content" width="620">
+                          <label class="addlabel-box" for="item_images_attributes_${id}_image" id="label-box-${id}">
+                            <pre class="label_box__text-visible">
+                              <svg area-hidden="true", class="image-upload-svg", fill-rule="evenodd", fill="222222", height="24", viewBox="0 0 24 24", width="24">
+                                <path d="M19.3,5H16.73l-.8-1.61A.7.7,0,0,0,15.3,3H8.7a.7.7,0,0,0-.63.39L7.27,5H4.7A2.7,2.7,0,0,0,2,7.7V18.3A2.7,2.7,0,0,0,4.7,21H19.3A2.7,2.7,0,0,0,22,18.3V7.7A2.7,2.7,0,0,0,19.3,5ZM12,17.3A4.3,4.3,0,1,1,16.3,13,4.31,4.31,0,0,1,12,17.3Z" />
+                              </svg>
+                            </pre>
+                          </label>
+                        </div>`;
+                        return labelhtml;
+                      }
+                      
+                      // ラベルの横幅を変える操作(6〜10枚目)
+    function addSetLabel() {
+      //プレビューの幅を変数に代入
+      var previewContent = $('.addlabel-content').prev(); 
+      //ラベル（カメラマークの範囲）のもともとの620pxからプレビューの分だけ引いた幅にする
+      addLabelWidth = (620 - $(previewContent).css('width').replace(/[^0-9]/g, '')); 
+      //ラベル（カメラマークの範囲）の幅を変える
+      $('.addlabel-content').css('width', addLabelWidth);
+    }
+
+    // 6〜10枚目用のプレビュー枠設置
+    function buildPreviewContent() {
+      var addPreviewContent = `<div class="preview-content"></div>`;
+      return addPreviewContent;
     }
 
     // 投稿編集時のみ（出品した画像情報を取得しておく必要がある）============================
@@ -35,7 +65,7 @@ $(function() {
         $('.label-content').hide();
       }
     }
-    
+    // =============================================================================
 
     // ラベルの横幅を変える操作
     function setLabel() {
@@ -46,118 +76,182 @@ $(function() {
       //ラベル（カメラマークの範囲）の幅を変える
       $('.label-content').css('width', labelWidth);
     }
-
+    
     const buildFileField = (id)=> {
       const html = `<div data-index="${id}" class="js-file_group">
-                      <input class="hidden-field" type="file"
-                      name="item[images_attributes][${id}][image]"
-                      id="item_images_attributes_${id}_image"
-                      <div class="js-remove">削除</div>
-                    </div>`;
+      <input class="hidden-field" type="file"
+      name="item[images_attributes][${id}][image]"
+      id="item_images_attributes_${id}_image"
+      <div class="js-remove">削除</div>
+      </div>`;
       return html;
     }
-    
-    //=============================================================================
-
     
     let fileIndex = [1,2,3,4,5,6,7,8,9,10];
     // プレビューの追加
     $(document).on('change', '.hidden-field', function() { 
       // file_fieldのnameに動的なindexをつける為の配列
-  
-      // $('#image-box').on('change', '.js-file', function(e) {
-      // fileIndexの先頭の数字を使ってinputを作る
-      $('.hidden-content').append(buildFileField(fileIndex[0]));
-      console.log(fileIndex[0]);
-      fileIndex.shift();
-      console.log(fileIndex[0]);
-      // 末尾の数に1足した数を追加する
-      fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
-      console.log(fileIndex[0]);
-
-  
-      setLabel(); //ラベルの横幅を変える処理を実行する
       
-      //js-file_groupのidの数値のみ取得
-      var id = $(this).attr('id').replace(/[^0-9]/g, '');
-      console.log(id[0]);
+        // fileIndexの先頭の数字を使ってinputを作る
+        $('.hidden-content').append(buildFileField(fileIndex[0]));
+        console.log(fileIndex[0]);
+        fileIndex.shift();
+        console.log(fileIndex[0]);
+        // 末尾の数に1足した数を追加する
+        fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+        console.log(fileIndex[0]);
+          
+          
+        // setLabel(); //ラベルの横幅を変える処理を実行する
 
-      //labelボックスのidとforを更新
-      const labellabel = $('.label-box').attr({id: `label-box--${id}`,for: `item_images_attributes_${id}_image`});
-      console.log(labellabel[0]); 
-      // debagger;
+        //inputタグのidの数値のみ取得
+        var id = $(this).attr('id').replace(/[^0-9]/g, '');
+        console.log(this); 
+        console.log(id); //6枚目を追加するときにここが4になるのはなぜ？4止まりなんだな。4以降が更新されてない。
+        
+        //labelボックスのidとforを更新
+        // const labellabel = $('.label-box').attr({id: `label-box--${id}`,for: `item_images_attributes_${id}_image`});
+        // console.log(labellabel[0]); 
+        // debagger;
+          
+        //選択したfileのオブジェクトを取得
+        var file = this.files[0];
+        //FileReaderオブジェクトの生成
+        var reader = new FileReader(); 
+        console.log(reader);
+        //readAsDataURLで指定したFileオブジェクトを読み込む
+        reader.readAsDataURL(file); 
+        //読み込み時に発火するイベント onloadメソッドは読み込みが完了したら実行する
+        reader.onload = function() {
+          //直前に実行したイベントが返した値を取得する
+          var image = this.result; 
+          console.log(image);
+          
+          previewLength = $(`#preview-box__${id}`).length
+          console.log(previewLength);
 
+          var index = $('.preview-box').length;
 
-      //選択したfileのオブジェクトを取得
-      var file = this.files[0];
-      //FileReaderオブジェクトの生成
-      var reader = new FileReader(); 
-      console.log(reader);
-      //readAsDataURLで指定したFileオブジェクトを読み込む
-      reader.readAsDataURL(file); 
-      //読み込み時に発火するイベント onloadメソッドは読み込みが完了したら実行する
-      reader.onload = function() {
-        //直前に実行したイベントが返した値を取得する
-        var image = this.result; 
-        console.log(image);
+          //inputタグと同じid値のプレビューがなかった場合はhtmlを追加
+          if ($(`#preview-box__${id}`).length == 0) {
+            previewLength = $(`#preview-box__${id}`).length
+            console.log(previewLength);
+            //プレビューの数を数えて変数に代入
+            var index = $('.preview-box').length; 
+            console.log(index);
+            //プレビューを生成する関数を変数に代入
+            var html = buildHTML(id); 
+            //プレビューエリアのhtmlを変数に代入
+            // var prevContent = $('.label-content').prev(); 
+            //既存のプレビューエリアに新たに追加されたプレビューを追加
 
+            //上段の時
+            if(index < 5){
+              $('.prev-content').append(html); 
+            }
 
-        //プレビューが元々なかった場合はhtmlを追加
-        if ($(`#preview-box__${id}`).length == 0) {
-          //プレビューの数を数えて変数に代入
-          var index = $('.preview-box').length; 
-          //プレビューを生成する関数を変数に代入
-          var html = buildHTML(id); 
-          //プレビューエリアのhtmlを変数に代入
-          var prevContent = $('.label-content').prev(); 
-          //既存のプレビューエリアに新たに追加されたプレビューを追加
-          $(prevContent).append(html); 
-        }
-        //画像を追加
-        $(`#preview-box__${id} img`).attr('src', `${image}`); 
-        var index = $('.preview-box').length; //preview-boxの数を数えて変数に代入
-        //プレビューが5個あったらラベルを隠す（ここはあとで10に変える）
-        if (index == 5) {
-          $('.label-content').hide();
-        }
+            //下段の時
+            if(index >= 5){
+              $('.preview-content').append(html); 
+            }
+          }
 
-        //プレビュー削除したフィールドにdestroy用のチェックボックスがあった場合、チェックを外す=====
-        if ($(`#item_images_attributes_${id}__destroy`)){
-          $(`#item_images_attributes_${id}__destroy`).prop('checked',false);
-        } 
+          // // 下段用のプレビューを設置
+          // if($(`#preview-box__${id}`).length == 1) {
+          //   console.log('hellohellohello');
+          //   var index = $('.preview-box').length; 
+          //   var html = buildHTML(id); 
+          //   console.log(id); //なぜここが4なんだ？
+          //   // var prevContent = $('.addlabel-content').prev(); 
+          //   $('.preview-content').append(html);
+          // }
+          
+          //画像を追加
+          $(`#preview-box__${id} img`).attr('src', `${image}`); 
+          var index = $('.preview-box').length; //preview-boxの数を数えて変数に代入
+          //プレビューが5個あったらラベルを隠す（ここはあとで10に変える）
+          if (index == 5) {
+            $('.label-content').hide();
+          }
+
+          //プレビュー削除したフィールドにdestroy用のチェックボックスがあった場合、チェックを外す=====
+          if ($(`#item_images_attributes_${id}__destroy`)){
+            $(`#item_images_attributes_${id}__destroy`).prop('checked',false);
+          } 
         //=============================================================================
 
-        // //ラベル（カメラマークの範囲）の横幅を変える処理を実行
-        setLabel(); 
-        // //ラベルのidとforの値を変更
-        if(index <= 5){
+        // // //ラベル（カメラマークの範囲）の横幅を変える処理を実行
+        // if (index <= 5) {
+          //   setLabel(); //1~5枚目の時の横幅を変える処理
+          // }
+          
+          // if (index >=6) {
+            //   addSetLabel(); //6~10枚目の時の横幅を変える処理
+            // }
+            
+            // //ラベルのidとforの値を変更
+            if(index < 5){
+              var newId = Number(id);
+              var newnewId = newId + 1;
+              //プレビューの数でラベルのオプションを更新する
+              // $('.label-box').attr({id: `label-box--${index}`,for: `item_images_attributes_${index}_image`}); //attrメソッドでid属性とfor属性を設定
+              $('.label-box').attr({id: `label-box--${newnewId}`,for: `item_images_attributes_${newnewId}_image`});
+              // const labelnum = $('.label-box').attr({id: `label-box--${index}`,for: `item_images_attributes_${index}_image`});
+              // console.log(labelnum[0]);
+              // const labelnum = $('.label-box').attr({id: `label-box--${id}`,for: `item_images_attributes_${id}_image`});
+              // console.log(labelnum[0]);
+              
+            }
+            
+            if(index == 5){
+              var newId = Number(id);
+              var newnewId = newId + 1;
+              $('.label-box').attr({id: `label-box--${newnewId}`,for: `item_images_attributes_${newnewId}_image`});
+              console.log(newnewId);
+              var addLabel = addLabelHTML(id); 
+              $('.label-content').after(addLabel); //6〜10枚目用のラベル設置
+              var previewContent = buildPreviewContent(); 
+              $('.addlabel-content').before(previewContent); //6〜10枚目用のプレビュー枠設置
+            }
+            
+            if(index > 5){
+              var newId = Number(id);
+              var newnewId = newId + 1;
+              $('.addlabel-box').attr({id: `label-box--${newnewId}`,for: `item_images_attributes_${newnewId}_image`});
+              console.log(newnewId);
+            }
+            
+            if(index == 10){
+              var newId = Number(id);
+              var newnewId = newId + 1;
+              $('.label-box').attr({id: `label-box--${newnewId}`,for: `item_images_attributes_${newnewId}_image`});
+              $('.addlabel-content').hide();
+            }
+            
 
-          var newId = Number(id);
-          var newnewId = newId + 1;
-          console.log(newnewId);
-          //プレビューの数でラベルのオプションを更新する
-          // $('.label-box').attr({id: `label-box--${index}`,for: `item_images_attributes_${index}_image`}); //attrメソッドでid属性とfor属性を設定
-          $('.label-box').attr({id: `label-box--${newnewId}`,for: `item_images_attributes_${newnewId}_image`});
-          // const labelnum = $('.label-box').attr({id: `label-box--${index}`,for: `item_images_attributes_${index}_image`});
-          // console.log(labelnum[0]);
-          // const labelnum = $('.label-box').attr({id: `label-box--${id}`,for: `item_images_attributes_${id}_image`});
-          // console.log(labelnum[0]);
-        }
-      }
-    });
-
-    // 画像の削除
-    $(document).on('click', '.delete-box', function() {
-      var index = $('.preview-box').length;
-      setLabel(index);
-      //item_images_attributes_${id}_image から${id}に入った数字のみを抽出
-      var id = $(this).attr('id').replace(/[^0-9]/g, '');
-      console.log(this);
-      //取得したidに該当するプレビューを削除
-      $(`#preview-box__${id}`).remove();
-
-      //新規登録時と編集時の場合分け==========================================================
-
+            // //ラベル（カメラマークの範囲）の横幅を変える処理を実行
+            if (index <= 5) {
+              setLabel(); //1~5枚目の時の横幅を変える処理
+            }
+            
+            if (index > 5) {
+              addSetLabel(); //6~10枚目の時の横幅を変える処理
+            }
+          }
+        });
+        
+        // 画像の削除
+        $(document).on('click', '.delete-box', function() {
+          var index = $('.preview-box').length;
+          setLabel(index);
+          //item_images_attributes_${id}_image から${id}に入った数字のみを抽出
+          var id = $(this).attr('id').replace(/[^0-9]/g, '');
+          console.log(this);
+          //取得したidに該当するプレビューを削除
+          $(`#preview-box__${id}`).remove();
+          
+          //新規登録時と編集時の場合分け==========================================================
+          
       
       
       //新規投稿時
@@ -181,6 +275,7 @@ $(function() {
           $('.label-content').show();
         }
         
+        //一番後ろのフォームのinputタグ(中身が入ってないフォーム)のidを取得
         var specialId = $(".js-file_group").last().children().attr('id').replace(/[^0-9]/g, '');
         console.log(specialId);
         
