16cf8eb1fe989c974a362ce75b3d5eecc1ffd040
naota7118 <naota7118@gmail.com>
2020-07-17 06:13:48 +0900

    edit unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,4 @@
 app_path = File.expand_path('../../../', __FILE__)
-shared_path = File.expand_path('../../../shared/', __FILE__)
 
 worker_processes 1
 # currentを指定
@@ -7,7 +6,7 @@ working_directory "#{app_path}/current"
 
 # それぞれ、sharedの中を参照するよう変更
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
-pid "#{shared_path}/tmp/pids/unicorn.pid"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
