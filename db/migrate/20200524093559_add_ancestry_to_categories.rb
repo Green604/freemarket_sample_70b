42f645b5aa9781b250a7731ebbee5deb634cd981
kyouheisunaga <sunagatheflame@gmail.com>
2021-09-06 14:18:13 +0900

    add ancestry comment out

--- db/migrate/20200524093559_add_ancestry_to_categories.rb
+++ db/migrate/20200524093559_add_ancestry_to_categories.rb
@@ -1,5 +1,5 @@
 class AddAncestryToCategories < ActiveRecord::Migration[5.2]
   def change
-    add_column :categories, :ancestry, :string
+    # add_column :categories, :ancestry, :string
   end
 end
