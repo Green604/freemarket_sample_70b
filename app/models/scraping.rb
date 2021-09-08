c9c9e2d6f7e2c119467e3e7192cd6364b0888b51
kyouheisunaga <sunagatheflame@gmail.com>
2021-09-08 21:24:58 +0900

    Update scraping.rb

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,14 +1,14 @@
-class Scraping < ApplicationRecord
-  require 'mechanize'
+# class Scraping < ApplicationRecord
+#   require 'mechanize'
 
-  agent = Mechanize.new
-  page = agent.get("https://www.mercari.com/jp/brand/")
-  elements = page.search('p')
+#   agent = Mechanize.new
+#   page = agent.get("https://www.mercari.com/jp/brand/")
+#   elements = page.search('p')
 
-  elements.each do |ele|
-    brand = Brand.new
-    brand.name = ele.inner_text
-    brand.save
-  end
+#   elements.each do |ele|
+#     brand = Brand.new
+#     brand.name = ele.inner_text
+#     brand.save
+#   end
 
-end
+# end
